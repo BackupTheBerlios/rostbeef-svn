b1fd6450adf2f07649a72275d769ff04b939c337
flatline <flatline@28a356de-c558-0410-bb61-c5a530a18edc>
2008-10-09 19:17:04 +0000

    initial commit

--- mod_rostbeef/test_xmlrpc_echothis.py
+++
@@ -1,9 +0,0 @@
-import xmlrpclib
-server_url = "http://127.0.0.1:4560";
-server = xmlrpclib.Server(server_url);
-params = {}
-params["user"] = "ggeo"
-params["host"] = "localhost"
-params["password"] = "gogo11"
-result = server.echothis("Hello World")
-print result
